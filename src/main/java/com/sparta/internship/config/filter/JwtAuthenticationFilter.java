d344404b7b80711470c0fdcfef1e02853787f3af
Minwoo <alsdn2606@gmail.com>
2025-01-19 13:35:37 +0900

    test: 로그인 기능 관련 테스트 추가
    - 요청받은 값에 따라 JwtUtil 클래스에 정의된 메서드들 테스트 완료
    - 토큰 생성 테스트를 통해 정상적인 토큰이 생성되는 것을 확인
    - 토큰에서 사용자 정보를 추출하는 테스트 성공
    - 토큰에 들어있는 정보를 검사하는 validateToken 메서드 확인 완료

--- src/main/java/com/sparta/internship/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/internship/config/filter/JwtAuthenticationFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
